0068989b4584f42915ad8097c908b50a7f57af82
arshita <varunanand@Varuns-MacBook-Pro.local>
2022-11-25 12:20:16 +0530

    openapi-docs

--- src/main/java/com/restapi/RestServiceApplication.java
+++ src/main/java/com/restapi/RestServiceApplication.java
@@ -1,11 +1,14 @@
 package com.restapi;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //https://andriperera.medium.com/create-a-rest-api-in-spring-boot-with-mysql-b250ff3aaa9b
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title="Promotion API",version="3.0",description="Promotion API"))
 public class RestServiceApplication {
 
     public static void main(String[] args) {
